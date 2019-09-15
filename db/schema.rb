035f1d6ac66c2a385efcc187c5ae9ba6b79f7be0
Dominick Licciardi <dominick.nola@gmail.com>
2019-09-14 18:36:14 -0600

    scaffold Car

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_002118) do
+ActiveRecord::Schema.define(version: 2019_09_15_003400) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string "make"
+    t.string "model"
+    t.string "vin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoice_parts", force: :cascade do |t|
     t.string "make"
